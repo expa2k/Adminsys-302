f8524ad96310352a81a45d990fa9b091d5d8fc2e
Andres Ruben Cota Buelna <144655928+expa2k@users.noreply.github.com>
2025-02-12 00:58:55 -0700

    pepito
    esto es grasa

---
+++ dnss.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+DOMINIO="reprobados.com"
+IP_SERVIDOR="192.168.0.155"
+
+NAMED_CONF="/etc/bind/named.conf.local"
+ZONE_FILE="/etc/bind/db.reprobados"
+
+echo "Instalando bind9..."
+
+sudo apt update && sudo apt install -y bind9 bind9-utils bind9-dnsutils
+
+echo "configurando named.conf.local..."
+
+sudo tee $NAMED_CONF > /dev/null <<EOL
+zone "$DOMINIO" {
+    type master;
+    file "$ZONE_FILE";
+};
+EOL
+
+echo "Creando archivo de zona..."
+sudo tee $ZONE_FILE > /dev/null <<EOL
+\$TTL 604800
+@   IN  SOA ns.$DOMINIO. admin.$DOMINIO. (
+        2        ; Serial
+        604800   ; Refresh
+        86400    ; Retry
+        2419200  ; Expire
+        604800   ; Negative Cache TTL
+);
+    IN  NS  ns.$DOMINIO.
+ns  IN  A   $IP_SERVIDOR
+www IN  A   $IP_SERVIDOR
+EOL
+
+echo "Verificando configuración de bind9"
+sudo named-checkconf
+sudo named-checkzone $DOMINIO $ZONE_FILE
+
+echo "Reiniciando bind9..."
+sudo systemctl restart bind9
+
+echo "Configuración DNS completada"
+echo "Verifica con: nslookup reprobados.com $IP_SERVIDOR"
