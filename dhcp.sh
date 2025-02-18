5b0a0e5a3d3bace1e7097e307420297fe96f05fc
Andres Ruben Cota Buelna <144655928+expa2k@users.noreply.github.com>
2025-02-17 23:09:49 -0700

    dhcp.sh
    cambios

--- dhcp.sh
+++ dhcp.sh
@@ -4,10 +4,10 @@
 sudo apt update
 sudo apt install -y isc-dhcp-server
 
-# Detectar la interfaz de red con dirección IP en 192.168.x.x
+# Detectar la interfaz de red con dirección en 192.168.x.x
 INTERFACE=$(ip -o -4 addr show | awk '$4 ~ /^192\.168\./ {print $2; exit}')
 echo "Usando la interfaz de red: $INTERFACE"
-echo "INTERFACESv4=\"$INTERFACE\"" | sudo tee /etc/default/isc-dhcp-server > /dev/null
+echo "INTERFACESv4=\"$INTERFACE\"" | sudo tee -a /etc/default/isc-dhcp-server > /dev/null
 
 # Solicitar rango de IPs
 read -p "Ingrese la IP inicial del rango DHCP: " RANGE_START
@@ -18,13 +18,14 @@ cat <<EOF | sudo tee /etc/dhcp/dhcpd.conf > /dev/null
 default-lease-time 600;
 max-lease-time 7200;
 subnet 192.168.1.0 netmask 255.255.255.0 {
-    range $RANGE_START $RANGE_END;
+    range ${RANGE_START} ${RANGE_END};
     option routers 192.168.1.1;
     option domain-name-servers 8.8.8.8, 8.8.4.4;
 }
 EOF
 
-# Reiniciar y habilitar el servicio DHCP
+# Recargar y reiniciar el servicio DHCP
+sudo systemctl daemon-reload
 sudo systemctl restart isc-dhcp-server
 sudo systemctl enable isc-dhcp-server
 
